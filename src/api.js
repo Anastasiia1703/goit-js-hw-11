a258418db3e458b508c58766148217b4c8b5a560
Anastasiia1703 <ostrov270418@gmail.com>
2023-05-02 18:39:49 +0300

    hw js 11

---
+++ src/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const ENDPOINT = 'https://pixabay.com/api/';
+
+export default class FetchGallery {
+  constructor() {
+    this.page = 1;
+    this.searchQuery = '';
+  }
+
+  async getData() {
+    const URL = `${ENDPOINT}?key=33725877-d5048a8c1b414c12f4e9c329f&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+
+    const response = await axios.get(URL);
+
+    this.nextPage();
+
+    return {
+      hits: response.data.hits,
+      totalHits: response.data.totalHits,
+    };
+  }
+
+  nextPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
